121e52fd4bc049240beed9bf5f5a9519b3b9b014
Palaptin <>
2024-01-10 16:07:50 +0100

    implemented an Service to get the heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import {HEROES} from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -10,10 +10,21 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   
+  constructor(private heroService: HeroService) {}
+
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 }
