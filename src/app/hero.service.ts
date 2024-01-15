8f9874055b32f367b63b3971935806650942ed0a
Palaptin <>
2024-01-15 10:28:02 +0100

    implemented an http call to an potential data server to get the heroes list

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,19 +3,24 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService,private http: HttpClient) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
   
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
